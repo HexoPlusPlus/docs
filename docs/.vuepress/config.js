75c1b4b1c09d6daf4f8a9e40e300a96508d5476e
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2021-04-21 19:24:57 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,32 @@
 module.exports = {
-  title: 'HexoPlusPlus',
-  description: 'A Powerful&Beautiful Hexo Dashboard',
-  head: [
-    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/HexoPlusPlus/CDN@master/doc_img/icon.png' }],
-  ],
-  markdown: {
-    lineNumbers: true
-  },
-  themeConfig: {
-    sidebarDepth: 2,
-    lastUpdated: '最后更新'
-  },
-  themeConfig: {
-    nav:[
-      { text: '快速上手', link: '/start/' },
-      { text: '常见问题', link: '/faq/' },
-      {
-        text: '如何使用',
-        items: [
-          { text: '正常玩法', link: '/use/use' },
-          { text: '内置API',link: '/use/api'},
-	{ text: '适配Hexo',link: '/use/hexoinit'},{ text: '插件列表',link: '/use/plug'},{ text: '主题支持',link: '/use/theme'}
+    title: 'HexoPlusPlus',
+    description: 'A Powerful&Beautiful Hexo Dashboard',
+    head: [
+        ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/HexoPlusPlus/CDN@master/doc_img/icon.png' }],
+        ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1878991317600808", async: true }]
+
+    ],
+
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        sidebarDepth: 2,
+        lastUpdated: '最后更新'
+    },
+    themeConfig: {
+        nav: [
+            { text: '快速上手', link: '/start/' },
+            { text: '常见问题', link: '/faq/' },
+            {
+                text: '如何使用',
+                items: [
+                    { text: '正常玩法', link: '/use/use' },
+                    { text: '内置API', link: '/use/api' },
+                    { text: '适配Hexo', link: '/use/hexoinit' }, { text: '插件列表', link: '/use/plug' }, { text: '主题支持', link: '/use/theme' }
+                ]
+            },
+            { text: '项目地址', link: 'https://github.com/HexoPlusPlus/HexoPlusPlus' }, { text: 'QQ群', link: 'https://jq.qq.com/?_wv=1027&k=rAcnhzqK' }
         ]
-      },
-       { text: '项目地址', link: 'https://github.com/HexoPlusPlus/HexoPlusPlus' },{ text: 'QQ群', link: 'https://jq.qq.com/?_wv=1027&k=rAcnhzqK' }
-    ]
-  }
+    }
 };
